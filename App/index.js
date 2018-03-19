96c323be29486734151702701e70ecada4c09b29
Waleed <wmfarooqi05@gmail.com>
2018-03-19 20:44:13 +0500

    WIP : Google maps markers

--- App/index.js
+++ App/index.js
@@ -9,14 +9,16 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View, Dimensions
 } from 'react-native';
 import { connect } from 'react-redux';
 
 import Components from './global/Components';
 
-import MapView from 'react-native-maps';
+import MapView, { Marker, ProviderPropType } from 'react-native-maps';
 
+const flagBlueImg = {uri:'http://icons.iconarchive.com/icons/paomedia/small-n-flat/1024/map-marker-icon.png'};
+const flagPinkImg =  {uri:'http://www.clker.com/cliparts/G/R/Y/M/S/g/map-pin-yellow.svg'};
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
@@ -26,18 +28,43 @@ const instructions = Platform.select({
 });
 
 type Props = {};
+
+
+
+const { width, height } = Dimensions.get('window');
+
+const ASPECT_RATIO = width / height;
+const LATITUDE = 37.78825;
+const LONGITUDE = -122.4324;
+const LATITUDE_DELTA = 0.0922;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const SPACE = 0.01;
+
+
 class App extends Component<Props> {
 
   
   constructor(props){
     super(props);
     this.state = { 
+      marker1: true,
+      marker2: false,
       region : {
         latitude: 37.78825,
         longitude: -122.4324,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
+      markers : [
+        {
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+          title: 'Waleed Farooqi',
+          subtitle: '1234 Foo Drive'
+        }
+      ],
     };
   }
 
@@ -52,8 +79,8 @@ class App extends Component<Props> {
     };
   }
   
-  onRegionChange(region) {
-    this.setState({ region : region });
+  onRegionChange(_region) {
+    this.setState({ region : _region });
   }
   
   render() {
@@ -61,10 +88,49 @@ class App extends Component<Props> {
       <View style={styles.container}>
         <Text>Waleed</Text>
         <MapView
-        style={{flex:1, height: 400, width:700}}
-          region={this.state.region}
-          onRegionChange={this.onRegionChange}
-        />
+          provider={this.props.provider}
+          style={{height : height * .4, width : width}}
+          initialRegion={{
+            latitude: LATITUDE,
+            longitude: LONGITUDE,
+            latitudeDelta: LATITUDE_DELTA,
+            longitudeDelta: LONGITUDE_DELTA,
+          }}
+        >
+        <Marker
+            onPress={() => this.setState({ marker1: !this.state.marker1 })}
+            coordinate={{
+              latitude: LATITUDE + SPACE,
+              longitude: LONGITUDE + SPACE,
+            }}
+            centerOffset={{ x: -18, y: -60 }}
+            anchor={{ x: 0.69, y: 1 }}
+            image={this.state.marker1 ? flagBlueImg : flagPinkImg}
+          >
+            <Text style={styles.marker}>X</Text>
+          </Marker>
+          <Marker
+            onPress={() => this.setState({ marker2: !this.state.marker2 })}
+            coordinate={{
+              latitude: LATITUDE - SPACE,
+              longitude: LONGITUDE - SPACE,
+            }}
+            centerOffset={{ x: -42, y: -60 }}
+            anchor={{ x: 0.84, y: 1 }}
+            image={this.state.marker2 ? flagBlueImg : flagPinkImg}
+          />
+          <Marker
+            onPress={() => this.setState({ marker2: !this.state.marker2 })}
+            coordinate={{
+              latitude: LATITUDE + SPACE,
+              longitude: LONGITUDE - SPACE,
+            }}
+            centerOffset={{ x: -42, y: -60 }}
+            anchor={{ x: 0.84, y: 1 }}
+            opacity={0.6}
+            image={this.state.marker2 ? flagBlueImg : flagPinkImg}
+          />
+        </MapView>
         <Text>Farooqi</Text>
       </View>
     );
